242248270abc173a0d5ed7469a7cffdb9d702328
chen.xia <1102827461@qq.com>
2024-03-15 13:34:27 +0800

    feat(*): 阶段性提交，开发中；

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { provide, reactive, ref } from 'vue'
+import { provide, reactive, ref, onRenderTracked, onRenderTriggered } from 'vue'
 import ComponentA from '@/components/ComponentA.vue'
 import ComponentB from '@/components/ComponentB.vue'
 import type { InjectionKey } from 'vue'
@@ -12,15 +12,21 @@ const c = {
   a: ComponentA,
   b: ComponentB
 }
+const count = ref(1)
+onRenderTracked((e) => {})
+onRenderTriggered((e) => {})
+const disabled = ref(false)
 </script>
 
 <template>
   <div id="test">
-    <TransitionGroup name="fade" tag="ul">
+    <div>{{ count }}</div>
+    <button class="add" @click="count++">count++</button>
+    <!-- <TransitionGroup name="fade" tag="ul">
       <li v-for="(item, index) in list" :key="item">
         {{ item }}
       </li>
-    </TransitionGroup>
+    </TransitionGroup> -->
     <button @click="list.splice(1, 0, 99)">pop</button>
     <KeepAlive exclude="ComponentA">
       <component :is="c[key]"></component>
@@ -29,11 +35,31 @@ const c = {
       <div class="modal">123</div>
     </Teleport>
     <button @click="key === 'a' ? (key = 'b') : (key = 'a')">toggle</button>
-    <div class="list-item" v-for="(item, index) in scrollList" :key="index">{{ item }}</div>
+    <br />
+    <br />
+    <br />
+    <!-- <div class="list-item" v-for="(item, index) in scrollList" :key="index">{{ item }}</div> -->
+    <div :class="{ shake: disabled }">
+      <button @click="disabled = true">click me</button>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+.shake {
+  animation: doshake 1s linear infinite;
+}
+@keyframes doshake {
+  0%,
+  50%,
+  100% {
+    transform: translateX(-10px);
+  }
+  25%,
+  75% {
+    transform: translateX(10px);
+  }
+}
 .fade-move,
 .fade-enter-active,
 .face-leave-active {
